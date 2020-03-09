ca701212d4e3762dab27f60652bb583ced43fead
Vince Varga <vince.varga@smaho.com>
2020-03-09 15:54:03 +0100

    Migrate to AndroidX

--- example/android/app/src/main/java/com/smaho/eng/esptouchexample/MainActivity.java
+++ example/android/app/src/main/java/com/smaho/eng/esptouchexample/MainActivity.java
@@ -7,9 +7,9 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.MethodCall;
